14094dff7597ce043e4ce373c3d9bd4b66861ca2
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-06-30 10:54:09 +0530

    create career page

---
+++ company-repo/career.js
@@ -0,0 +1,56 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const value_cards = gsap.utils.toArray(".value-card");
+
+value_cards.forEach((card, index) => {
+    if (index === 0) return;
+
+    gsap.from(card, {
+        yPercent: 100,
+        ease: "none",
+        scrollTrigger: {
+            trigger: card,
+            start: "top bottom",
+            end: "top top",
+            scrub: 1.5,
+        },
+    });
+});
+
+
+
+// const story_cards = gsap.utils.toArray(".story-section-card");
+
+// story_cards.forEach((card, index) => {
+//     if (index === 0) return;
+
+//     gsap.from(card, {
+//         yPercent: 100,
+//         ease: "none",
+//         scrollTrigger: {
+//             trigger: card,
+//             start: "top bottom",
+//             end: "top top",
+//             scrub: 1.5,
+//         },
+//     });
+// });
+
+
+
+const cards = gsap.utils.toArray(".story-card-inner");
+
+cards.forEach((card, index) => {
+    if (index === 0) return;
+
+    gsap.from(card, {
+        yPercent: 100,
+        ease: "none",
+        scrollTrigger: {
+            trigger: card.parentElement,
+            start: "top bottom",
+            end: "top top",
+            scrub: 1.5,
+        },
+    });
+});
